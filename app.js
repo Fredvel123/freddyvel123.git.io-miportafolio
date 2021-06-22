b74e29a7b8e99ffc9c0f63cd2e0b983d6aa721d3
freddy <freddyvelarde59@gmail.com>
2021-06-22 16:13:34 -0430

    primera version de mi portafolio

---
+++ app.js
@@ -0,0 +1,31 @@
+// Tomamos variables
+
+let click = document.querySelector(".click");
+let icono = document.querySelector(".icono");
+let menu = document.querySelector(".menu")
+let abierto = false;
+// creamos la funciuon para abrir y cerrar el menu
+
+let openClose = () =>{
+    if(abierto == false){
+        menu.classList.add("menu2");
+        abierto= true;
+    } else if (abierto == true){
+        menu.classList.remove("menu2");
+        abierto=false;
+    }
+}
+click.addEventListener("click", openClose)
+
+// codigo para cerrar tocando cualquier parte de la pantalla
+
+let closeWindow= (e) =>{
+    placeOpen = e.target;
+    if(abierto){
+        if(placeOpen !== click){
+            openClose();
+            abierto=false
+        }
+    }
+}
+window.addEventListener("click", closeWindow)
